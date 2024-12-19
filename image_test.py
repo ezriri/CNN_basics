a89e93f17e2704081a08d42f3170450f46265fa8
ezriri <ezrielizabeth@hotmail.com>
2024-12-19 21:43:52 +0000

    testing VS code (i hate it)

--- image_test.py
+++ image_test.py
@@ -3,5 +3,7 @@
 from PIL import Image
 
 image_loc = '/gws/nopw/j04/dcmex/users/ezriab/image_labelling/test_random_sample/1749002_23ch1.png'
+#bit_closer_image = '/Users/ezri/181384_30ch0.png'
 img = Image.open(image_loc)
+#img = Image.open(bit_closer_image)
 img.show() 
